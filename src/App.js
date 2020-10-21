aad7b9afb9ea10935812661260457c3d5e4cf328
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-20 18:00:53 -0700

    added notes

--- src/App.js
+++ src/App.js
@@ -8,15 +8,37 @@ const App = () => {
 // STATE ===========================================================================
   const [ personsState, setPersonsState ] = useState({
     persons: [
-            {name: 'Ethan', age: 6},
-            {name: 'Chloe', age: 3},
-            {name: 'Kirby', age: 1}
+            {id: 128943, name: 'Ethan', age: 6},
+            {id: 635648, name: 'Chloe', age: 3},
+            {id: 998384, name: 'Kirby', age: 1}
           ]
   });
   
   const [ showPersons, setShowpersons ] = useState(false);
 
 //  EVENT HANDLERS =================================================================
+  const nameInputHandler = (event, id) => {
+    // Check if "id" arguement passed in matches with person's id
+    const personIndex = personsState.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // Access person by personIndex
+    const person = {
+      ...personsState.persons[personIndex]
+    };
+
+    // Assign dynamic value and update name
+    person.name = event.target.value;
+
+    // Update 1 index, value of person being assigned to persons[personIndex]
+    const persons = [...personsState.persons];
+    persons[personIndex] = person;
+    
+    // Update Array, State
+    setPersonsState( {persons: persons})
+  }
+  
   const togglePersonHandler = () => {
     setShowpersons(!showPersons);
   }
@@ -46,7 +68,12 @@ const App = () => {
     personsDiv = (
       <div >
         {personsState.persons.map((person, index) => {
-          return <Person key={index} click={() => deletePersonHandler(index)} name={person.name} age={person.age} />
+          return <Person 
+          key={person.id} 
+          click={() => deletePersonHandler(index)} 
+          name={person.name} 
+          age={person.age}
+          inputName={(event) => nameInputHandler(event, person.id)} />
         })}
       </div>
     )
